cfd6945538de03f8b624b57bdb7982970a29df46
Robson de Moraes Silva <rms2307@outlook.com>
2024-11-16 01:44:09 -0300

    Implementa geração do QR Code

--- EazyQRCodeGeneratorApp/Pages/Home.razor.cs
+++ EazyQRCodeGeneratorApp/Pages/Home.razor.cs
@@ -1,6 +1,24 @@
-﻿namespace EazyQRCodeGeneratorApp.Pages
+﻿using QRCoder;
+using System.Drawing;
+
+namespace EazyQRCodeGeneratorApp.Pages
 {
     public partial class Home
     {
+        private string SourceUrl { get; set; } = string.Empty;
+        private string? QrCodeBase64 { get; set; }
+        private int Size { get; set; } = 15;
+
+        private readonly string ImageType = "data:image/png;base64, ";
+
+        private void GenerateQrCode()
+        {
+            using QRCodeGenerator qrGenerator = new();
+            using QRCodeData qrCodeData = qrGenerator.CreateQrCode(SourceUrl, QRCodeGenerator.ECCLevel.Q);
+            using PngByteQRCode qrCode = new(qrCodeData);
+            byte[] qrCodeImage = qrCode.GetGraphic(Size, Color.Black, Color.White);
+
+            QrCodeBase64 = $"{ImageType}{Convert.ToBase64String(qrCodeImage)}";
+        }
     }
 }
